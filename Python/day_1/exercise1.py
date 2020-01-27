7ec8b487ac0489bbd51074e33de7308ba705e6e7
Haaaaaaaaaaaa <2693636326@qq.com>
2020-01-27 10:17:48 +0800

    Python Exercise 2020-1-27

--- Python/day_1/exercise1.py
+++ Python/day_1/exercise1.py
@@ -1,1 +1,57 @@
-print("Hello World!")
+'''
+Day_1 Exercise_1
+Date:2020-1-25
+'''
+# print(("Hello"+ "World!"+'1')*2,end="")
+# print ('hell"oword"')
+# # str='roob'
+# print(str[0:2])
+
+#input("\n\n按下 enter 键后退出。")
+
+#多行代码写在一行用：分开。
+#import sys; x = 'runoob'; sys.stdout.write(x + '\n')
+
+
+# a=1
+# b=3
+# c=2
+# if a<b:
+#     print("a<b")
+# else:
+#     print("c=2")
+
+
+# import sys
+# print('================Python import mode==========================')
+# print ('命令行参数为:')
+# for i in sys.argv:
+#     print (i)
+# print ('\n python 路径为',sys.path)
+
+"""
+String转化为int，先转化为float
+"""
+# a=2.1
+# b='2.1'
+# print(a+1)
+# print(int(float(b))+1)
+
+"""
+使用引号表示多行字符串
+"""
+# str="""hello------------------------------------------------------------------------------------------------------------
+# world"""
+# print(str)
+
+'''
+变量练习
+'''
+# a=b=c=1
+# print(a+b+c)
+
+# a,b,c=1,2,3
+# print(a)
+# print(b )
+# print(c )
+
